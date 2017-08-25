d63da893b3c765268a28fb81389ec2952f0b4823
Prajesh Ananthan <prajesh.ananthan@outlook.com>
2017-08-26 03:30:42 +0800

    Fix on naming convention

--- spring-mvc/src/main/java/io/prajesh/service/security/SpringSecurityUserDetailsService.java
+++ spring-mvc/src/main/java/io/prajesh/service/security/SpringSecurityUserDetailsService.java
@@ -32,7 +32,7 @@ public class SpringSecurityUserDetailsService implements UserDetailsService {
   }
 
   @Override
-  public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
-    return userUserDetailsConverter.convert(userService.findUserByUserName(userName));
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    return userUserDetailsConverter.convert(userService.findUserByUserName(username));
   }
 }
